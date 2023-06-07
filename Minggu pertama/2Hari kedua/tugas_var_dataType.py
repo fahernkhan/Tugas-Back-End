bbf5e6855ee58439e3af8345734ea0e23e1231e8
UmarBinKhatab <fathurrahmanhernanda123@gmail.com>
2023-06-07 23:33:33 +0700

    Initial commit

---
+++ Minggu pertama/2Hari kedua/tugas_var_dataType.py
@@ -0,0 +1,56 @@
+"""<h2>Soal 1: Hello World to Python</h2>
+    
+Print Say Hello World to Python
+"""
+
+
+
+"""Expected Output : 
+
+Hello World Python
+
+<h2>Soal 2: Aritmatika di Python </h2>
+    
+- Buat statement pertambahan antara 2 number di Python
+- Buat statement perkurangan antara 2 number di Python
+- Buat statement perkalian antara 2 number di Python
+- Buat statement pembagian antara 2 number di Python
+"""
+
+
+
+"""<h2>Quiz 3: Assign Variable dan Tipe Data Integer, Float </h2>
+
+- Buat suatu variabel a dan b, dimana a dan b adalah nilai bertipe data numeric
+- Berikan suatu nilai bertipe data integer, hasil pembagian dari a dengan b
+- Berikan suatu nilai bertipe data float, hasil pembagian dari a dengan b
+"""
+
+
+
+"""<h2>Soal 4: String Operation </h2>
+
+- masukan nama depan kamu kedalam suatu variable firstname
+- masukan nama belakang kamu kedalam suatu variable lastname
+- tampilkan suatu kalimat 'Hello sanbercode, saya firstname lastname! saya siap belajar python backend development.'
+"""
+
+
+
+"""Expected Output :
+
+Hello sanbercode, saya Afrida Hafizhatul Ulum! saya siap belajar python backend development.
+
+<h2>Soal 5: Tipe Data</h2>
+
+Lengkapi code di bawah ini untuk menghasilkan output yang sesuai
+
+usia =
+kata = 'usiaku: '
+print(kata + ...(usia)+....)
+"""
+
+"""Expected Output : (sesuai usia masing-masing)
+
+usiaku: 28 tahun
+"""
