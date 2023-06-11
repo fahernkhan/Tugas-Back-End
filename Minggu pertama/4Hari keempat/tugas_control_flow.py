4d8f41a5da4a5d105af9507f0536c09450dde9fe
UmarBinKhatab <fathurrahmanhernanda123@gmail.com>
2023-06-11 11:18:02 +0700

    nambah tugas

--- Minggu pertama/4Hari keempat/tugas_control_flow.py
+++ Minggu pertama/4Hari keempat/tugas_control_flow.py
@@ -4,7 +4,17 @@ Lengkapi kode untuk menghasilkan suatu output yang di harapkan
 
 - Bualah sebuah if-else statement yang dimana akan mem-print 'High' jika grade adalah 'A' dan price lebih dari 100000, kemudian mem-print 'Medium' jika grade adalah 'A' dan price lebih dari 50000 dan memprint 'low' jika grade adalah 'A' dan price lebih kecil dan sama dengan 50000.
 '''
+grade = 'A'  # Ganti dengan nilai grade yang sesuai
+price = 60000  # Ganti dengan nilai price yang sesuai
 
+if grade == 'A':
+    if price > 100000:
+        print('High')
+    elif price > 50000:
+        print('Medium')
+    else:
+        print('Low')
+print("")
 #kerjakan di bawah ini
 
 '''Soal 2: For Loop
@@ -20,7 +30,7 @@ Expected output:
 
 #kerjakan di bawah ini
 
-data_siswa = [
+'''data_siswa = [
     {
         "nama":"Budi",
         "nilai": 90
@@ -50,4 +60,57 @@ data_siswa = [
         "nilai": 76
     }
 ]
+'''
+#kerjakan di bawah ini
+
+'''Soal 2: For Loop
+
+Lengkapi kode untuk menghasilkan suatu output yang diharapkan:
+*   Cari siswa mana saja yang memiliki nilai lebih dari sama dengan 80. Masukkan kedalam sebuah list. print hasilnya
+
+Expected output:
+
+['Budi', 'Rudi', 'Leo']
+
+'''
+
+#kerjakan di bawah ini
+
+data_siswa = [
+    {
+        "nama": "Budi",
+        "nilai": 90
+    },
+    {
+        "nama": "Nina",
+        "nilai": 78
+    },
+    {
+        "nama": "Rudi",
+        "nilai": 91
+    },
+    {
+        "nama": "Olivia",
+        "nilai": 76
+    },
+    {
+        "nama": "Leo",
+        "nilai": 80
+    },
+    {
+        "nama": "Liam",
+        "nilai": 67
+    },
+    {
+        "nama": "Sheila",
+        "nilai": 76
+    }
+]
+
+siswa_lebih_80 = []
+for siswa in data_siswa:
+    if siswa['nilai'] >= 80:
+        siswa_lebih_80.append(siswa['nama'])
+
+print(siswa_lebih_80)
 
--- Minggu pertama/5Hari kelima/tugas_fungsi_class.py
+++ Minggu pertama/5Hari kelima/tugas_fungsi_class.py
@@ -7,13 +7,23 @@ Expected Output:
 12.39
 '''
 
-obj_list = [11.25, 18.0, 20.0, 10.75, 9.50, 13.45, 23.0, 7.0, 8.45, 2.50]
+'''obj_list = [11.25, 18.0, 20.0, 10.75, 9.50, 13.45, 23.0, 7.0, 8.45, 2.50]
 def mean_list(inp_list):
     pass
     #lengkapi kode ini
  
 
 print(mean_list(obj_list))
+'''
+obj_list = [11.25, 18.0, 20.0, 10.75, 9.50, 13.45, 23.0, 7.0, 8.45, 2.50]
+
+def mean_list(inp_list):
+    total = sum(inp_list)
+    mean = total / len(inp_list)
+    return mean
+
+print(mean_list(obj_list))
+
 
 '''
 Soal 2. Mapping Fungsi lambda
@@ -23,9 +33,15 @@ Expected output:
 500
 '''
 
-obj_list = [i for i in range(1000)]
+'''obj_list = [i for i in range(1000)]
 
 #lengkapi kode ini dengan fungsi lambda yang diaplikasikan pada obj_list
 
+
+print(hasil)
+'''
+obj_list = [i for i in range(1000)]
+
+hasil = sum(filter(lambda x: x % 2 == 0, obj_list))
 
 print(hasil)
