25f425f24b38e11322f1516f29363b8ac8cd80f0
UmarBinKhatab <fathurrahmanhernanda123@gmail.com>
2023-06-11 10:41:06 +0700

    nambah file

---
+++ Minggu pertama/4Hari keempat/tugas_control_flow.py
@@ -0,0 +1,53 @@
+'''Soal 1: If-Else Statement
+
+Lengkapi kode untuk menghasilkan suatu output yang di harapkan
+
+- Bualah sebuah if-else statement yang dimana akan mem-print 'High' jika grade adalah 'A' dan price lebih dari 100000, kemudian mem-print 'Medium' jika grade adalah 'A' dan price lebih dari 50000 dan memprint 'low' jika grade adalah 'A' dan price lebih kecil dan sama dengan 50000.
+'''
+
+#kerjakan di bawah ini
+
+'''Soal 2: For Loop
+
+Lengkapi kode untuk menghasilkan suatu output yang diharapkan:
+*   Cari siswa mana saja yang memiliki nilai lebih dari sama dengan 80. Masukkan kedalam sebuah list. print hasilnya
+
+Expected output:
+
+['Budi', 'Rudi', 'Leo']
+
+'''
+
+#kerjakan di bawah ini
+
+data_siswa = [
+    {
+        "nama":"Budi",
+        "nilai": 90
+    },
+    {
+        "nama":"Nina",
+        "nilai": 78
+    },
+    {
+        "nama":"Rudi",
+        "nilai": 91
+    },
+    {
+        "nama":"Olivia",
+        "nilai": 76
+    },
+    {
+        "nama":"Leo",
+        "nilai": 80
+    },
+    {
+        "nama":"Liam",
+        "nilai": 67
+    },
+    {
+        "nama":"Sheila",
+        "nilai": 76
+    }
+]
+
